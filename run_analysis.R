6b949caa299858e9c9b7203747bf2a2fd30db844
Khar <slaweksobon@gmail.com>
2015-06-21 15:55:24 -0700

    Cleaning the script

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
-#test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-#train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-#names <- read.table("./data/UCI HAR Dataset/features.txt")
+test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+names <- read.table("./data/UCI HAR Dataset/features.txt")
 activitiesNames <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 testActivities <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 trainActivities <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
@@ -17,7 +17,6 @@ combined <- rbind(testAct, trainAct)
 colnames(combined)[1:2] <- c("subject", "activity")
 meansAndStds <- combined[, grep("^(subject)|(activity)|(.*(mean[(][)]|std[(][)]).*)$", names(combined))]
 
-#summary <- tapply(meansAndStds[, 3], meansAndStds[, 1:2], mean)
 library("stats")
 newFrame <- aggregate(meansAndStds, list(subject=meansAndStds[,1], activity=meansAndStds[,2]), mean)[c(-3, -4)]
 write.table(newFrame, row.name = FALSE)
